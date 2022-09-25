4f4b2763539290a1fff12dbee96eb7c5f805ca88
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-09-25 23:16:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,13 @@ const fName = document.querySelector('#fullname')
 const email = document.querySelector('#email')
 const errorMsg = document.querySelector('#error-msg')
 
-function 
+function onSubmit(e){
+  e.preventDefault;
+  
+  if(fName.value === '' || email.value === ''){
+    errorMsg.classList.add('error');
+    errorMsg.innerText = 'Fields are empty. Please enter all fields';
+    setTimeout(() => errorMsg.remove(), 3000);
+  }
+
+}
