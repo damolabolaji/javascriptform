0d7d343e85498f9300691e84ba4e11a3f6aaebd7
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-09-25 23:45:33 +0100

    Update main.js
    created list items in the ul

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const fName = document.querySelector('#fullname')
 const email = document.querySelector('#email')
 const errorMsg = document.querySelector('#error-msg')
+const userList = document.querySelector('#users')
+
+document.querySelector('form').addEventListener('submit', onSubmit)
 
 function onSubmit(e){
   e.preventDefault;
@@ -9,6 +12,16 @@ function onSubmit(e){
     errorMsg.classList.add('error');
     errorMsg.innerText = 'Fields are empty. Please enter all fields';
     setTimeout(() => errorMsg.remove(), 3000);
+  }
+  else{
+    const li = document.createElement('li');
+    
+    li.appendChild(document.createTextNode(`${fName.value} : ${email,value}`));
+    
+    userList.appendChild('li');
+    
+    fName.value = '';
+    email.value === '';
   }
 
 }
